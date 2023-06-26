b00147e2cdb1c979998f9e47f8ed9e6fad0a6f77
Raymond Wright <rayw1710@gmail.com>
2023-06-27 00:50:30 +0100

    Move Midi->Song conversion to detail class

--- include/sightread/midiparser.hpp
+++ include/sightread/midiparser.hpp
@@ -4,9 +4,7 @@
 #include <cstdint>
 #include <set>
 #include <span>
-#include <string>
 
-#include "sightread/detail/midi.hpp"
 #include "sightread/hopothreshold.hpp"
 #include "sightread/metadata.hpp"
 #include "sightread/song.hpp"
@@ -15,20 +13,17 @@
 namespace SightRead {
 class MidiParser {
 private:
-    std::string m_song_name;
-    std::string m_artist;
-    std::string m_charter;
+    SightRead::Metadata m_metadata;
     SightRead::HopoThreshold m_hopo_threshold;
     std::set<SightRead::Instrument> m_permitted_instruments;
     bool m_permit_solos;
 
 public:
-    explicit MidiParser(const SightRead::Metadata& metadata);
+    explicit MidiParser(SightRead::Metadata metadata);
     MidiParser& hopo_threshold(SightRead::HopoThreshold hopo_threshold);
     MidiParser&
     permit_instruments(std::set<SightRead::Instrument> permitted_instruments);
     MidiParser& parse_solos(bool permit_solos);
-    SightRead::Song from_midi(const SightRead::Detail::Midi& midi) const;
     SightRead::Song parse(std::span<const std::uint8_t> data) const;
 };
 }
---
+++ src/sightread/detail/midiconverter.hpp
@@ -0,0 +1,33 @@
+#ifndef SIGHTREAD_DETAIL_MIDICONVERTER_HPP
+#define SIGHTREAD_DETAIL_MIDICONVERTER_HPP
+
+#include <set>
+#include <string>
+
+#include "sightread/detail/midi.hpp"
+#include "sightread/hopothreshold.hpp"
+#include "sightread/metadata.hpp"
+#include "sightread/song.hpp"
+#include "sightread/songparts.hpp"
+
+namespace SightRead::Detail {
+class MidiConverter {
+private:
+    std::string m_song_name;
+    std::string m_artist;
+    std::string m_charter;
+    SightRead::HopoThreshold m_hopo_threshold;
+    std::set<SightRead::Instrument> m_permitted_instruments;
+    bool m_permit_solos;
+
+public:
+    explicit MidiConverter(SightRead::Metadata metadata);
+    MidiConverter& hopo_threshold(SightRead::HopoThreshold hopo_threshold);
+    MidiConverter&
+    permit_instruments(std::set<SightRead::Instrument> permitted_instruments);
+    MidiConverter& parse_solos(bool permit_solos);
+    SightRead::Song convert(const SightRead::Detail::Midi& midi) const;
+};
+}
+
+#endif
