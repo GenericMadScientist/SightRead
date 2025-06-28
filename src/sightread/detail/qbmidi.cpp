4697dff116f489713e728686217dea81794697b7
Raymond Wright <gms@gmscorner.dev>
2025-06-28 15:10:34 +0100

    Refactor away span passing in QbReader

--- src/sightread/detail/qbmidi.cpp
+++ src/sightread/detail/qbmidi.cpp
@@ -7,66 +7,94 @@ namespace {
 class QbReader {
 private:
     SightRead::Detail::Endianness m_endianness;
+    std::span<const std::uint8_t> m_full_file;
+    std::span<const std::uint8_t> m_remaining_data;
 
-    float read_float(std::span<const std::uint8_t>& data) const
+    void advance_bytes(std::size_t byte_count)
+    {
+        m_remaining_data = m_remaining_data.subspan(byte_count);
+    }
+
+    void snap_to_four_byte_alignment()
+    {
+        const auto bytes_read = m_full_file.size() - m_remaining_data.size();
+        advance_bytes((~bytes_read + 1) & 0b11);
+    }
+
+    void move_to_position(std::size_t position)
+    {
+        m_remaining_data = m_full_file.subspan(position);
+    }
+
+    float read_float()
     {
         float value = 0;
         switch (m_endianness) {
         case SightRead::Detail::Endianness::BigEndian:
-            value = read_four_byte_be<float>(data, 0);
+            value = read_four_byte_be<float>(m_remaining_data, 0);
             break;
         case SightRead::Detail::Endianness::LittleEndian:
-            value = read_four_byte_le<float>(data, 0);
+            value = read_four_byte_le<float>(m_remaining_data, 0);
             break;
         }
-        data = data.subspan(4);
+        advance_bytes(4);
         return value;
     }
 
-    std::int32_t read_int32(std::span<const std::uint8_t>& data) const
+    std::int32_t read_int32()
     {
         std::int32_t value = 0;
         switch (m_endianness) {
         case SightRead::Detail::Endianness::BigEndian:
-            value = read_four_byte_be<std::int32_t>(data, 0);
+            value = read_four_byte_be<std::int32_t>(m_remaining_data, 0);
             break;
         case SightRead::Detail::Endianness::LittleEndian:
-            value = read_four_byte_le<std::int32_t>(data, 0);
+            value = read_four_byte_le<std::int32_t>(m_remaining_data, 0);
             break;
         }
-        data = data.subspan(4);
+        advance_bytes(4);
         return value;
     }
 
-    std::uint32_t read_uint32(std::span<const std::uint8_t>& data) const
+    std::uint32_t read_le_uint32()
+    {
+        const auto value
+            = read_four_byte_le<std::uint32_t>(m_remaining_data, 0);
+        advance_bytes(4);
+        return value;
+    }
+
+    std::uint32_t read_uint32()
     {
         std::uint32_t value = 0;
         switch (m_endianness) {
         case SightRead::Detail::Endianness::BigEndian:
-            value = read_four_byte_be<std::uint32_t>(data, 0);
+            value = read_four_byte_be<std::uint32_t>(m_remaining_data, 0);
             break;
         case SightRead::Detail::Endianness::LittleEndian:
-            value = read_four_byte_le<std::uint32_t>(data, 0);
+            value = read_four_byte_le<std::uint32_t>(m_remaining_data, 0);
             break;
         }
-        data = data.subspan(4);
+        advance_bytes(4);
         return value;
     }
 
-    std::uint16_t read_uint16(std::span<const std::uint8_t>& data) const
+    std::uint16_t read_uint16()
     {
         std::uint16_t value = 0;
         switch (m_endianness) {
         case SightRead::Detail::Endianness::BigEndian:
-            value = read_two_byte_be<std::uint16_t>(data, 0);
+            value = read_two_byte_be<std::uint16_t>(m_remaining_data, 0);
             break;
         case SightRead::Detail::Endianness::LittleEndian:
-            value = read_two_byte_le<std::uint16_t>(data, 0);
+            value = read_two_byte_le<std::uint16_t>(m_remaining_data, 0);
             break;
         }
-        data = data.subspan(2);
+        advance_bytes(2);
         return value;
     }
+
+    std::uint8_t read_uint8() { return pop_front(m_remaining_data); }
 
     SightRead::Detail::QbItemType struct_item_type(std::uint8_t byte) const
     {
@@ -93,68 +121,61 @@ private:
         }
     }
 
-public:
-    QbReader(SightRead::Detail::Endianness endianness)
-        : m_endianness {endianness}
+    std::vector<std::any> read_array_node()
     {
-    }
-
-    std::vector<std::any> read_array_node(std::span<const std::uint8_t>& data,
-                                          std::uint32_t file_size) const
-    {
-        const auto first_item = read_item_info(data);
-        const auto item_count = read_uint32(data);
+        const auto first_item = read_item_info();
+        const auto item_count = read_uint32();
         std::vector<std::any> array;
         array.reserve(item_count);
 
         switch (first_item.type) {
         case SightRead::Detail::QbItemType::StructFlag:
-            data = data.subspan(4);
+            advance_bytes(4);
             break;
         case SightRead::Detail::QbItemType::Integer: {
             if (item_count > 1) {
-                const auto list_start = read_uint32(data);
-                data = data.subspan(list_start + data.size() - file_size);
+                const auto list_start = read_uint32();
+                move_to_position(list_start);
             }
             for (auto i = 0U; i < item_count; ++i) {
-                array.emplace_back(read_uint32(data));
+                array.emplace_back(read_uint32());
             }
             break;
         }
         case SightRead::Detail::QbItemType::Struct: {
-            const auto list_start = read_uint32(data);
-            data = data.subspan(list_start + data.size() - file_size);
+            const auto list_start = read_uint32();
+            move_to_position(list_start);
             std::vector<std::uint32_t> start_list;
             if (item_count == 1) {
                 start_list.push_back(list_start);
             } else {
                 for (auto i = 0U; i < item_count; ++i) {
-                    start_list.push_back(read_uint32(data));
+                    start_list.push_back(read_uint32());
                 }
             }
 
             for (auto start : start_list) {
-                data = data.subspan(data.size() - file_size + start);
-                array.emplace_back(read_struct_data(data, file_size));
+                move_to_position(start);
+                array.emplace_back(read_struct_data());
             }
 
             break;
         }
         case SightRead::Detail::QbItemType::Array: {
-            const auto list_start = read_uint32(data);
-            data = data.subspan(list_start + data.size() - file_size);
+            const auto list_start = read_uint32();
+            move_to_position(list_start);
             std::vector<std::uint32_t> start_list;
             if (item_count == 1) {
                 start_list.push_back(list_start);
             } else {
                 for (auto i = 0U; i < item_count; ++i) {
-                    start_list.push_back(read_uint32(data));
+                    start_list.push_back(read_uint32());
                 }
             }
 
             for (auto start : start_list) {
-                data = data.subspan(data.size() - file_size + start);
-                array.emplace_back(read_array_node(data, file_size));
+                move_to_position(start);
+                array.emplace_back(read_array_node());
             }
 
             break;
@@ -172,49 +193,43 @@ public:
         return array;
     }
 
-    SightRead::Detail::QbHeader
-    read_header(std::span<const std::uint8_t>& data) const
+    SightRead::Detail::QbHeader read_header()
     {
         constexpr auto REST_OF_HEADER_SIZE = 20;
 
-        const auto flags = read_uint32(data);
-        const auto file_size = read_uint32(data);
-        data = data.subspan(REST_OF_HEADER_SIZE);
+        const auto flags = read_uint32();
+        const auto file_size = read_uint32();
+        advance_bytes(REST_OF_HEADER_SIZE);
         return {flags, file_size};
     }
 
-    SightRead::Detail::QbItem read_item(std::span<const std::uint8_t>& data,
-                                        std::uint32_t file_size) const
+    SightRead::Detail::QbItem read_item()
     {
-        const auto info = read_item_info(data);
-        const auto shared_props = read_shared_props(data, info.type);
-        const auto data_value
-            = read_value(data, file_size, info.type, shared_props.value);
+        const auto info = read_item_info();
+        const auto shared_props = read_shared_props(info.type);
+        const auto data_value = read_value(info.type, shared_props.value);
         return {info, shared_props, data_value};
     }
 
-    SightRead::Detail::QbItemInfo
-    read_item_info(std::span<const std::uint8_t>& data) const
+    SightRead::Detail::QbItemInfo read_item_info()
     {
-        const auto info = read_four_byte_le<std::uint32_t>(data, 0);
+        const auto info = read_le_uint32();
         const auto flags = static_cast<std::uint8_t>(info >> 8);
         const auto type
             = static_cast<SightRead::Detail::QbItemType>(info >> 16);
-        data = data.subspan(4);
         return {flags, type};
     }
 
     SightRead::Detail::QbSharedProps
-    read_shared_props(std::span<const std::uint8_t>& data,
-                      SightRead::Detail::QbItemType type) const
+    read_shared_props(SightRead::Detail::QbItemType type)
     {
-        const auto id = read_uint32(data);
-        const auto qb_name = read_uint32(data);
+        const auto id = read_uint32();
+        const auto qb_name = read_uint32();
 
         SightRead::Detail::QbSharedProps props {id, qb_name, {}};
         switch (type) {
         case SightRead::Detail::QbItemType::Array:
-            props.value = read_uint32(data);
+            props.value = read_uint32();
             break;
         case SightRead::Detail::QbItemType::Float:
         case SightRead::Detail::QbItemType::Integer:
@@ -229,24 +244,23 @@ public:
                 + std::to_string(static_cast<int>(type)));
         }
 
-        data = data.subspan(4);
+        advance_bytes(4);
         return props;
     }
 
-    std::any read_simple_value(std::span<const std::uint8_t>& data,
-                               SightRead::Detail::QbItemType type) const
+    std::any read_simple_value(SightRead::Detail::QbItemType type)
     {
         switch (type) {
         case SightRead::Detail::QbItemType::Integer:
-            return read_int32(data);
+            return read_int32();
         case SightRead::Detail::QbItemType::Float:
-            return read_float(data);
+            return read_float();
         case SightRead::Detail::QbItemType::Pointer:
         case SightRead::Detail::QbItemType::QbKey:
         case SightRead::Detail::QbItemType::String:
         case SightRead::Detail::QbItemType::Struct:
         case SightRead::Detail::QbItemType::WideString:
-            return read_uint32(data);
+            return read_uint32();
         case SightRead::Detail::QbItemType::Array:
         case SightRead::Detail::QbItemType::StructFlag:
         default:
@@ -256,12 +270,12 @@ public:
         }
     }
 
-    std::string read_string(std::span<const std::uint8_t>& data) const
+    std::string read_string()
     {
         std::string value;
 
         while (true) {
-            const auto character = pop_front(data);
+            const auto character = read_uint8();
             if (character == 0) {
                 return value;
             }
@@ -269,28 +283,25 @@ public:
         }
     }
 
-    SightRead::Detail::QbStructData
-    read_struct_data(std::span<const std::uint8_t>& data,
-                     std::uint32_t file_size) const
+    SightRead::Detail::QbStructData read_struct_data()
     {
-        const auto header_marker = read_uint32(data);
-        const auto item_offset = read_uint32(data);
+        const auto header_marker = read_uint32();
+        const auto item_offset = read_uint32();
         std::vector<SightRead::Detail::QbStructItem> items;
         auto next_item = item_offset;
 
         while (next_item != 0) {
-            data = data.subspan(next_item + data.size() - file_size);
-            items.push_back(read_struct_item(data, file_size));
+            move_to_position(next_item);
+            items.push_back(read_struct_item());
             next_item = items.back().props.next_item;
         }
 
         return {header_marker, item_offset, std::move(items)};
     }
 
-    SightRead::Detail::QbStructInfo
-    read_struct_info(std::span<const std::uint8_t>& data) const
+    SightRead::Detail::QbStructInfo read_struct_info()
     {
-        const auto info = read_four_byte_le<std::uint32_t>(data, 0);
+        const auto info = read_le_uint32();
         const auto flags = static_cast<std::uint8_t>(info >> 8);
         auto info_byte = flags;
         const auto info_byte_2 = static_cast<std::uint8_t>(info >> 16);
@@ -298,36 +309,29 @@ public:
             info_byte = info_byte_2;
         }
         const auto type = struct_item_type(info_byte & 0x7F);
-        data = data.subspan(4);
         return {flags, type};
     }
 
-    SightRead::Detail::QbStructItem
-    read_struct_item(std::span<const std::uint8_t>& data,
-                     std::uint32_t file_size) const
+    SightRead::Detail::QbStructItem read_struct_item()
     {
-        const auto info = read_struct_info(data);
-        const auto props = read_struct_props(data, info.type);
-        const auto data_value
-            = read_value(data, file_size, info.type, props.value);
+        const auto info = read_struct_info();
+        const auto props = read_struct_props(info.type);
+        const auto data_value = read_value(info.type, props.value);
 
         return {info, props, data_value};
     }
 
     SightRead::Detail::QbStructProps
-    read_struct_props(std::span<const std::uint8_t>& data,
-                      SightRead::Detail::QbItemType type) const
+    read_struct_props(SightRead::Detail::QbItemType type)
     {
-        const auto id = read_uint32(data);
-        const auto value = read_simple_value(data, type);
-        const auto next_item = read_uint32(data);
+        const auto id = read_uint32();
+        const auto value = read_simple_value(type);
+        const auto next_item = read_uint32();
         return {id, value, next_item};
     }
 
-    std::any read_value(std::span<const std::uint8_t>& data,
-                        std::uint32_t file_size,
-                        SightRead::Detail::QbItemType type,
-                        const std::any& default_value) const
+    std::any read_value(SightRead::Detail::QbItemType type,
+                        const std::any& default_value)
     {
         std::any value;
 
@@ -339,16 +343,16 @@ public:
             value = default_value;
             break;
         case SightRead::Detail::QbItemType::Array:
-            value = read_array_node(data, file_size);
+            value = read_array_node();
             break;
         case SightRead::Detail::QbItemType::String:
-            value = read_string(data);
+            value = read_string();
             break;
         case SightRead::Detail::QbItemType::Struct:
-            value = read_struct_data(data, file_size);
+            value = read_struct_data();
             break;
         case SightRead::Detail::QbItemType::WideString:
-            value = read_widestring(data);
+            value = read_widestring();
             break;
         case SightRead::Detail::QbItemType::StructFlag:
         default:
@@ -357,22 +361,43 @@ public:
                 + std::to_string(static_cast<int>(type)));
         }
 
-        data = data.subspan((data.size() - file_size) % 4);
+        snap_to_four_byte_alignment();
 
         return value;
     }
 
-    std::wstring read_widestring(std::span<const std::uint8_t>& data) const
+    std::wstring read_widestring()
     {
         std::wstring value;
 
         while (true) {
-            const auto character = read_uint16(data);
+            const auto character = read_uint16();
             if (character == 0) {
                 return value;
             }
             value.push_back(character);
         }
+    }
+
+public:
+    QbReader(std::span<const std::uint8_t> file,
+             SightRead::Detail::Endianness endianness)
+        : m_endianness {endianness}
+        , m_full_file {file}
+        , m_remaining_data {file}
+    {
+    }
+
+    SightRead::Detail::QbMidi parse()
+    {
+        const auto header = read_header();
+        std::vector<SightRead::Detail::QbItem> items;
+
+        while (!m_remaining_data.empty()) {
+            items.push_back(read_item());
+        }
+
+        return {header, items};
     }
 };
 }
@@ -381,13 +406,6 @@ SightRead::Detail::QbMidi
 SightRead::Detail::parse_qb(std::span<const std::uint8_t> data,
                             Endianness endianness)
 {
-    const auto reader = QbReader(endianness);
-    const auto header = reader.read_header(data);
-    std::vector<SightRead::Detail::QbItem> items;
-
-    while (!data.empty()) {
-        items.push_back(reader.read_item(data, header.file_size));
-    }
-
-    return {header, items};
+    QbReader reader {data, endianness};
+    return reader.parse();
 }
